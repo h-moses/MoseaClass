35ce6fef7f0c241ac10cc08940ec0ab0f9ef4172
h_admin <709820314@qq.com>
2022-01-09 13:01:14 +0800

    finish test fragment

--- app/src/main/java/com/hyt/moseaclass/adapters/AnswerAdapter.java
+++ app/src/main/java/com/hyt/moseaclass/adapters/AnswerAdapter.java
@@ -1,34 +1,46 @@
 package com.hyt.moseaclass.adapters;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import androidx.annotation.DrawableRes;
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.hyt.moseaclass.R;
-import com.hyt.moseaclass.components.OptionView;
 import com.hyt.moseaclass.data.entity.TestQuestion;
 import com.hyt.moseaclass.databinding.ItemAnswerBinding;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class AnswerAdapter extends RecyclerView.Adapter<AnswerAdapter.ViewHolder> {
 
+    private static final String TAG = AnswerAdapter.class.getSimpleName();
+    private final Map<String, Integer> singleMap = new HashMap<>();
+    private final Map<String, Integer> judgeMap = new HashMap<>();
     private List<TestQuestion> questionList = new ArrayList<>();
-    @DrawableRes
-    private final int[] singleOptions = {R.drawable.optiona, R.drawable.optionb, R.drawable.optionc, R.drawable.optiond};
-    @DrawableRes
-    private final int[] judgeOptions = {R.drawable.ic_correct, R.drawable.ic_fault};
+
     private final Context mContext;
+
+    private int totalScore = 0;
 
     public AnswerAdapter(Context mContext,List<TestQuestion> questions) {
         this.mContext = mContext;
         this.questionList = questions;
+        singleMap.put("A",0);
+        singleMap.put("B",1);
+        singleMap.put("C",2);
+        singleMap.put("D",3);
+        judgeMap.put("正确",0);
+        judgeMap.put("错误",1);
     }
 
     @NonNull
@@ -44,22 +56,43 @@ public class AnswerAdapter extends RecyclerView.Adapter<AnswerAdapter.ViewHolder
         List<String> options = questionList.get(position).getOptions();
         if (questionList.get(position).getType().equals("single")) {
             for (int i = 0; i < options.size(); i++) {
-                OptionView optionView = new OptionView(mContext);
-                optionView.setData(singleOptions[i], options.get(i));
-                holder.getBinding().optionsLayout.addView(optionView);
+                RadioButton radioButton = new RadioButton(mContext);
+                radioButton.setText(options.get(i));
+                radioButton.setTextColor(ContextCompat.getColorStateList(mContext, R.color.radio_btn_text_selector));
+                holder.getBinding().optionsGroup.addView(radioButton);
             }
         } else {
             for (int i = 0; i < options.size(); i++) {
-                OptionView optionView = new OptionView(mContext);
-                optionView.setData(judgeOptions[i], options.get(i));
-                holder.getBinding().optionsLayout.addView(optionView);
+                RadioButton radioButton = new RadioButton(mContext);
+                radioButton.setText(options.get(i));
+                radioButton.setTextColor(ContextCompat.getColorStateList(mContext,R.color.radio_btn_text_selector));
+                holder.getBinding().optionsGroup.addView(radioButton);
             }
         }
+        holder.getBinding().optionsGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                if (radioGroup.getChildCount() == 2) {
+                    if (i == radioGroup.getChildAt(judgeMap.get(questionList.get(position).getAnswer())).getId()) {
+                        totalScore += questionList.get(position).getScore();
+                    }
+                } else if (radioGroup.getChildCount() == 4) {
+                    if (i == radioGroup.getChildAt(singleMap.get(questionList.get(position).getAnswer())).getId()) {
+                        totalScore += questionList.get(position).getScore();
+                    }
+                }
+                Log.e(TAG, "onCheckedChanged: " + totalScore);
+            }
+        });
     }
 
     @Override
     public int getItemCount() {
         return questionList.size();
+    }
+
+    public int calcScore() {
+        return totalScore;
     }
 
     static class ViewHolder extends RecyclerView.ViewHolder {
--- app/src/main/java/com/hyt/moseaclass/ui/course/AnswerActivity.java
+++ app/src/main/java/com/hyt/moseaclass/ui/course/AnswerActivity.java
@@ -1,12 +1,19 @@
 package com.hyt.moseaclass.ui.course;
 
+import android.content.DialogInterface;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.View;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
+import com.hyt.moseaclass.R;
 import com.hyt.moseaclass.adapters.AnswerAdapter;
 import com.hyt.moseaclass.data.entity.TestQuestion;
 import com.hyt.moseaclass.databinding.ActivityAnswerBinding;
@@ -30,12 +37,44 @@ public class AnswerActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         handleIntent();
+        setCustomNavigationIcon(R.color.white);
 
+        AnswerAdapter answerAdapter = new AnswerAdapter(this, questionList);
         binding.answerRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-        binding.answerRecyclerView.setAdapter(new AnswerAdapter(this,questionList));
+        binding.answerRecyclerView.setAdapter(answerAdapter);
+
+        binding.btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int score = answerAdapter.calcScore();
+                new AlertDialog.Builder(view.getContext()).setTitle("测试得分").setMessage("恭喜你，本次测试得分为".concat(String.valueOf(score))).setPositiveButton("确认", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                }).create().show();
+                binding.btnSubmit.setEnabled(false);
+                binding.btnSubmit.setText("已提交");
+                binding.btnSubmit.setTextColor(view.getResources().getColor(R.color.gray_text));
+                binding.btnSubmit.setBackgroundColor(view.getResources().getColor(R.color.gray_back));
+            }
+        });
     }
 
     private void handleIntent() {
         questionList = (List<TestQuestion>) getIntent().getSerializableExtra("data");
     }
+    /*
+     * 根据状态栏的展开折叠修改返回按钮的颜色
+     * */
+    private void setCustomNavigationIcon(int color) {
+        Drawable upArrow = ContextCompat.getDrawable(this, androidx.appcompat.R.drawable.abc_ic_ab_back_material);
+        if (upArrow != null) {
+            upArrow.setColorFilter(ContextCompat.getColor(this, color), PorterDuff.Mode.SRC_ATOP);
+            if (getSupportActionBar() != null) {
+                getSupportActionBar().setHomeAsUpIndicator(upArrow);
+            }
+        }
+    }
+
 }
